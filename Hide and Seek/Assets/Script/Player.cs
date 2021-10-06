db0afe7af561fb4ef346f6298beb6bf0df22aed2
misosiruda <81164760+misosiruda@users.noreply.github.com>
2021-10-06 19:40:07 +0900

    coffineir
    add coffineir

--- Hide and Seek/Assets/Script/Player.cs
+++ Hide and Seek/Assets/Script/Player.cs
@@ -6,8 +6,9 @@ public class Player : MonoBehaviour
 {
     public Transform player;
     public CharacterController plcon;
-    public Transform camera;
+    public Transform plCamera;
     public float speed;
+    private bool isPlaiavle = true;
 
     private float hAxis;
     private float vAxis;
@@ -15,9 +16,21 @@ public class Player : MonoBehaviour
     private static float gravity;
 
     public LayerMask doorLM;
-    public Transform shojiDoor;
-    private static float time;
-    static WaitForSeconds waitDoor;
+    private Transform shojiDoor;
+    private float time;
+    static WaitForSeconds wait60FPS;
+    private static bool isDoorMoving = false;
+
+    public LayerMask closetLM;
+    public LayerMask inClosetLM;
+    public LayerMask closetOutLM;
+    private static Transform cloDoor;
+    private Transform closet;
+    private Transform outClo;
+    private static bool isInCloset = false;
+
+    public LayerMask chiffonierLM;
+    private Transform chfnier;
 
     public void Move()
     {
@@ -31,8 +44,8 @@ public class Player : MonoBehaviour
         Vector2 moveInput = new Vector2(hAxis, vAxis);
         bool isMove = moveInput.magnitude != 0;
         //카메라 전면
-        Vector3 lookForward = new Vector3(camera.forward.x, 0f, camera.forward.z).normalized;
-        Vector3 lookRight = new Vector3(camera.right.x, 0f, camera.right.z).normalized;
+        Vector3 lookForward = new Vector3(plCamera.forward.x, 0f, plCamera.forward.z).normalized;
+        Vector3 lookRight = new Vector3(plCamera.right.x, 0f, plCamera.right.z).normalized;
         Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
 
         player.forward = lookForward;
@@ -53,58 +66,172 @@ public class Player : MonoBehaviour
 
     public IEnumerator DoorInterection()
     {
-        RaycastHit hitinfo;
-        if (Physics.Raycast(player.position, player.forward, out hitinfo, 1f, doorLM))
+        if (!isDoorMoving)
+        { 
+            RaycastHit hitinfo;
+            if (Physics.Raycast(plCamera.position, plCamera.forward, out hitinfo, 2f, doorLM))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    isDoorMoving = true;
+                    time = 0;
+                    shojiDoor = hitinfo.transform;
+                    if (shojiDoor.localPosition.x == -0.053f)
+                    {
+                        if (shojiDoor.localPosition.z < 0.85f)
+                        {
+                            while (time < 30)
+                            {
+                                shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.053f, 0f, 0.85f), 0.2f);
+                                time++;
+                                yield return wait60FPS;
+                            }
+                            shojiDoor.localPosition = new Vector3(-0.053f, 0f, 0.85f);
+                        }
+                        else
+                        {
+                            while (time < 30)
+                            {
+                                shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.053f, 0f, 0f), 0.2f);
+                                time++;
+                                yield return wait60FPS;
+                            }
+                            shojiDoor.localPosition = new Vector3(-0.053f, 0f, 0f);
+                        }
+                    }
+                    else
+                    {
+                        if (shojiDoor.localPosition.z > -0.85f)
+                        {
+                            while (time < 30)
+                            {
+                                shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.009f, 0f, -0.85f), 0.2f);
+                                time++;
+                                yield return wait60FPS;
+                            }
+                            shojiDoor.localPosition = new Vector3(-0.009f, 0f, -0.85f);
+                        }
+                        else
+                        {
+                            while (time < 30)
+                            {
+                                shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.009f, 0f, 0f), 0.2f);
+                                time++;
+                                yield return wait60FPS;
+                            }
+                            shojiDoor.localPosition = new Vector3(-0.009f, 0f, 0f);
+                        }
+                    }
+                    isDoorMoving = false;
+                }
+            }
+        }
+    }
+
+    public IEnumerator ClosetInterection()
+    {
+        RaycastHit hitout;
+        if (!isInCloset)
+        {
+            RaycastHit hitDoor;
+            if (Physics.Raycast(plCamera.position, plCamera.forward, out hitDoor, 2f, closetLM))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    isInCloset = true;
+                    time = 0;
+                    cloDoor = hitDoor.transform;
+                    isPlaiavle = false;
+                    while(time<21)
+                    {
+                        cloDoor.localRotation = Quaternion.Euler(-90, 90 / 18 * time, 0);
+                        time++;
+                        yield return wait60FPS;
+                    }
+                    time = 0;
+                    RaycastHit hitClo;
+                    Physics.SphereCast(player.position - player.forward, 1f, player.forward, out hitClo, 2f, inClosetLM);
+                    closet = hitClo.transform;
+                    while (time<10)
+                    {
+                        player.Rotate(0, (180 / 8 * time), 0);
+                        player.position = Vector3.Lerp(player.position, new Vector3(closet.position.x, closet.position.y + 1.5f, closet.position.z), 0.3f);
+                        time++;
+                        yield return wait60FPS;
+                    }
+                    time = 0;
+                    while (time < 21)
+                    {
+                        cloDoor.localRotation = Quaternion.Euler(-90, 110 -(90 / 18 * time), 0);
+                        time++;
+                        yield return wait60FPS;
+                    }
+                    isPlaiavle = true;
+                }
+            }
+        }
+        else if(isPlaiavle && Physics.SphereCast(player.position - player.forward, 1f, player.forward, out hitout, 2f, closetOutLM))
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                isPlaiavle = false;
                 time = 0;
-                shojiDoor = hitinfo.collider.transform;
-                if(shojiDoor.localPosition.x == -0.053f)
+                while (time < 21)
                 {
-                    if (shojiDoor.localPosition.z < 0.85f)
+                    cloDoor.localRotation = Quaternion.Euler(-90, 90 / 18 * time, 0);
+                    time++;
+                    yield return wait60FPS;
+                }
+                time = 0;
+                outClo = hitout.transform;
+                while (time < 10)
+                {
+                    player.position = Vector3.Lerp(player.position, new Vector3(outClo.position.x, outClo.position.y, outClo.position.z), 0.3f);
+                    time++;
+                    yield return wait60FPS;
+                }
+                time = 0;
+                while (time < 21)
+                {
+                    cloDoor.localRotation = Quaternion.Euler(-90, 100 - (90 / 18 * time), 0);
+                    time++;
+                    yield return wait60FPS;
+                }
+                isPlaiavle = true;
+                isInCloset = false;
+            }
+        }
+    }
+
+    public IEnumerator ChiffonierInterection()
+    {
+        RaycastHit hitchfn;
+        if (Physics.Raycast(plCamera.position, plCamera.forward, out hitchfn, 2f, chiffonierLM))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                chfnier = hitchfn.transform;
+                if (chfnier.localPosition.z < 0.45f)
+                {
+                    time = 0;
+                    while (time < 21)
                     {
-                        while (time < 30)
-                        {
-                            shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.053f, 0f, 0.85f), 0.2f);
-                            time++;
-                            yield return waitDoor;
-                        }
-                        shojiDoor.localPosition = new Vector3(-0.053f, 0f, 0.85f);
+                        chfnier.localPosition = Vector3.Lerp(chfnier.localPosition, new Vector3(0f, chfnier.localPosition.y, 1.1f), 0.2f);
+                        time++;
+                        yield return wait60FPS;
                     }
-                    else
-                    {
-                        while (time < 30)
-                        {
-                            shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.053f, 0f, 0f), 0.2f);
-                            time++;
-                            yield return waitDoor;
-                        }
-                        shojiDoor.localPosition = new Vector3(-0.053f, 0f, 0f);
-                    }
+                    chfnier.localPosition = new Vector3(0f, chfnier.localPosition.y, 1.1f);
                 }
                 else
                 {
-                    if (shojiDoor.localPosition.z > -0.85f)
+                    time = 0;
+                    while (time < 21)
                     {
-                        while (time < 30)
-                        {
-                            shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.009f, 0f, -0.85f), 0.2f);
-                            time++;
-                            yield return waitDoor;
-                        }
-                        shojiDoor.localPosition = new Vector3(-0.009f, 0f, -0.85f);
+                        chfnier.localPosition = Vector3.Lerp(chfnier.localPosition, new Vector3(0f, chfnier.localPosition.y, 0.4f), 0.2f);
+                        time++;
+                        yield return wait60FPS;
                     }
-                    else
-                    {
-                        while (time < 30)
-                        {
-                            shojiDoor.localPosition = Vector3.Lerp(shojiDoor.localPosition, new Vector3(-0.009f, 0f, 0f), 0.2f);
-                            time++;
-                            yield return waitDoor;
-                        }
-                        shojiDoor.localPosition = new Vector3(-0.009f, 0f, 0f);
-                    }
+                    chfnier.localPosition = new Vector3(0f, chfnier.localPosition.y, 0.4f);
                 }
             }
         }
@@ -114,22 +241,30 @@ public class Player : MonoBehaviour
     void Start()
     {
         gravity = 1000f;
-        waitDoor = new WaitForSeconds(1f / 60f);
+        wait60FPS = new WaitForSeconds(1f / 60f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        if(isPlaiavle)
+        {
+            Move();
+        }
         StartCoroutine(DoorInterection());
+        StartCoroutine(ClosetInterection());
+        StartCoroutine(ChiffonierInterection());
     }
     private void LateUpdate()
     {
         Vector3 vector = player.position;
         vector.y = player.position.y;
-        camera.position = vector; 
+        plCamera.position = vector; 
         Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X") * 2, Input.GetAxis("Mouse Y") * 2);
-        Vector3 camAngle = camera.rotation.eulerAngles;
-        camera.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x, camAngle.z);
+        Vector3 camAngle = plCamera.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+        if (x < 180f) { x = Mathf.Clamp(x, -1f, 89f); }
+        else { x = Mathf.Clamp(x, 271f, 361f); }
+        plCamera.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
     }
 }
